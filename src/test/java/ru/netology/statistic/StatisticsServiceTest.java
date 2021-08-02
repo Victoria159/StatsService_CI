6f666bba91826c676f40eff4da3120b65867e91e
Victoria159 <v.s.bocha@gmail.com>
2021-08-02 07:09:00 +0300

    Changed StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,44 +10,11 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
-    }
-    @Test
-    void findMin() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
-
-        long actual = service.findMin(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-    @Test
-    void TotalSum () {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 85;
-
-        long actual = service.totalSum(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-    @Test
-    void shouldAverageSalesAmount() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 7;
-
-        long actual = service.averageSalesAmount(incomesInBillions);
-        assertEquals(expected, actual);
-
     }
 }
